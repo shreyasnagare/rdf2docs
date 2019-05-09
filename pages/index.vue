41a0fdc339ad5c33626d7fac8fb24bb6b922852f
Shreyas Nagare <shreyasnagare@gmail.com>
2019-05-09 01:47:32 -0400

    update index.vue
    add search component

--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,56 @@
 <template>
-  <section class="container">
+
+  <section class="container" v-breathing-colors="sample">
+    <vue-progress-bar></vue-progress-bar>
     <div>
-      <h1 class="title">
-        rdf2docs
-      </h1>
-      <h2 class="subtitle">
-        Generate docs from RDF
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
+      <div class="title">
+        BrickDocs
       </div>
+      <div class="subtitle">
+        Refer to the latest Brick concepts available
+      </div>
+      <search/>
     </div>
   </section>
 </template>
 
 <script>
+  // import Logo from '~/components/Logo.vue'
+  import Search from '~/components/Search.vue'
+  // import BootstrapVue from 'bootstrap-vue'
+  // import 'bootstrap/dist/css/bootstrap.css'
+  // import 'bootstrap-vue/dist/bootstrap-vue.css'
+  //
+  // Vue.use(BootstrapVue)
+  import Vue from 'vue';
+  import BreathingColors from 'vue-breathing-colors';
+
+  import VueTextareaAutosize from 'vue-textarea-autosize'
+  import VueProgressBar from 'vue-progressbar'
+
+  Vue.use(VueProgressBar, {
+  color: '#007bff',
+  failedColor: 'red',
+  height: '8px'
+})
+Vue.use(VueTextareaAutosize)
+Vue.use(BreathingColors);
 
 export default {
+  data() {
+    return {
+      sample: {
+        colors: ['white'],
+        interval: 3000,
+        transition: {
+          duration: 1000
+        }
+      }
+    }
+  },
   components: {
+    // Logo,
+    Search
   }
 }
 </script>
@@ -46,20 +70,29 @@ export default {
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   display: block;
   font-weight: 300;
-  font-size: 100px;
+  font-size: 6em;
   color: #35495e;
   letter-spacing: 1px;
+  background: -webkit-linear-gradient(#57a2ff, #49b3b3);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
 }
 
 .subtitle {
   font-weight: 300;
-  font-size: 42px;
+  font-size: 2em;
   color: #526488;
-  word-spacing: 5px;
+  /*word-spacing: 5px;*/
   padding-bottom: 15px;
+  margin-bottom: 16px;
 }
 
 .links {
   padding-top: 15px;
+}
+
+
+.v--modal-background-click{
+  background: #0579fc30;
 }
 </style>
